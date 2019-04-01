01635d3fb3b847d3d49d3f55e15c122f36255346
solucao <solucao@SRVTeste>
2019-04-01 10:00:27 -0400

    subindo telas

---
+++ src/br/com/principal/Evento.java
@@ -0,0 +1,33 @@
+
+package br.com.principal;
+
+public class Evento {
+    private int idvento;
+    private String nome;
+    private double data;
+
+    public int getIdvento() {
+        return idvento;
+    }
+
+    public void setIdvento(int idvento) {
+        this.idvento = idvento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getData() {
+        return data;
+    }
+
+    public void setData(double data) {
+        this.data = data;
+    }
+    
+}
---
+++ src/br/com/principal/Participantes.java
@@ -0,0 +1,24 @@
+
+package br.com.principal;
+
+public class Participantes {
+    private int idparticipante;
+    private double data;
+
+    public int getIdparticipante() {
+        return idparticipante;
+    }
+
+    public void setIdparticipante(int idparticipante) {
+        this.idparticipante = idparticipante;
+    }
+
+    public double getData() {
+        return data;
+    }
+
+    public void setData(double data) {
+        this.data = data;
+    }
+    
+}
